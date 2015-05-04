0c43d0b1fe7fe2c41353e5192def2f80889e31a0
Varatep Buranintu <varatep@buranintu.com>
2015-05-03 22:54:47 -0700

    update install script

--- installation.sh
+++ installation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-adduser binker
+sudo adduser binker
 sudo usermod -a -G sudo binker
 sudo apt-get install lamp-server^
 sudo nano/etc/mysql.cnf
@@ -12,7 +12,7 @@ mysql -u root -p << EOF
 use mysql;
 GRANT ALL PRIVILEGES ON *.* TO 'binker'@'localhost' IDENTIFIED BY 'admin' WITH GRANT OPTION;
 CREATE DATABASE Comp424;
-CREATE TABLE COMP424.students (userid INT AUTO_INCREMENT,today INT,username TEXT, fname TEXT, lname TEXT, email TEXT, birthday DATE, userpass TEXT);
+CREATE TABLE Comp424.students (userid INT AUTO_INCREMENT primary key,today INT,username TEXT, fname TEXT, lname TEXT, email TEXT, birthday DATE, userpass TEXT);
 SHOW TABLES;
 \q;
 EOF
