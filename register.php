b1e066c16dfdbd2cd253b19c96eafe57d5432df6
Varatep Buranintu <varatep@buranintu.com>
2015-05-03 23:14:10 -0700

    change db credentials

--- register.php
+++ register.php
@@ -5,7 +5,7 @@
 
 
 <link href='/assets/strength.css' rel="stylesheet" type="text/css">
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script type="text/javascript" src="./assets/jquery.min.js"></script>
 <script type="text/javascript" src="./assets/strength.js"></script>
 <script type="text/javascript" src="./assets/strength.min.js"></script>
 
